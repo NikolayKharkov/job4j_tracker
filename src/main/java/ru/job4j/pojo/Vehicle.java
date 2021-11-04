8f0a89980bbf190e48d302136a31f74fbf05b944
NikolayKharkov <vtiko564@gmail.com>
2021-11-04 16:25:51 +0300

    3.2. Зачем нужно приведение типов [#310019 #224201]

--- src/main/java/ru/job4j/pojo/Vehicle.java
+++ src/main/java/ru/job4j/pojo/Vehicle.java
@@ -2,14 +2,4 @@ package ru.job4j.pojo;
 
 public interface Vehicle {
     void move();
-
-    public static void main(String[] args) {
-        Vehicle airplane = new Airplane();
-        Vehicle train = new Train();
-        Vehicle bus = new Bus();
-        Vehicle[] vehicles = new Vehicle[]{airplane, train, bus};
-        for (Vehicle vehicle : vehicles) {
-            vehicle.move();
-        }
-    }
 }
