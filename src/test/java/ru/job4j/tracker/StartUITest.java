6525887ac985826cd26c9dcabdefdf0f71a8158c
NikolayKharkov <vtiko564@gmail.com>
2021-11-06 11:26:55 +0300

    9.2. Тесты вывода на консоль [#33585 #224619]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -63,11 +63,11 @@ public class StartUITest {
     }
 
     @Test
-    public void whenFindByNameActionTestOutputIsUnSuccessfully() {
+    public void whenFindByNameActionTestOutputIsSuccessfully() {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
-        String findName = "test2";
+        String findName = "test1";
         Input in = new StubInput(
                 new String[]{"0", findName, "1"}
         );
@@ -82,7 +82,7 @@ public class StartUITest {
                         + "0. Find items by name" + ln
                         + "1. Exit Program" + ln
                         + "=== Find items by name ===" + ln
-                        + "Заявки с именем: " + findName + " не найдены." + ln
+                        + one + ln
                         + "Menu." + ln
                         + "0. Find items by name" + ln
                         + "1. Exit Program" + ln
@@ -91,12 +91,12 @@ public class StartUITest {
     }
 
     @Test
-    public void whenFindByIdActionTestOutputIsUnSuccessfully() {
+    public void whenFindByIdActionTestOutputIsSuccessfully() {
         Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item one = tracker.add(new Item("test1"));
         Input in = new StubInput(
-                new String[]{"0", String.valueOf(6), "1"}
+                new String[]{"0", String.valueOf(1), "1"}
         );
         UserAction[] actions = new UserAction[]{
                 new FindAction(out),
@@ -109,7 +109,7 @@ public class StartUITest {
                         + "0. Find item by id" + ln
                         + "1. Exit Program" + ln
                         + "=== Find item by id ===" + ln
-                        + "Заявка с введенным id: " + String.valueOf(6) + " не найдена." + ln
+                        + one + ln
                         + "Menu." + ln
                         + "0. Find item by id" + ln
                         + "1. Exit Program" + ln
