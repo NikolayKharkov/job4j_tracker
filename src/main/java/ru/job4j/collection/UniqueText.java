97486d6feacdaea8dd1f57b4ba48299dfbb03e51
NikolayKharkov <vtiko564@gmail.com>
2021-11-08 21:29:10 +0300

    0.5. Джекпот в казино

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.Arrays;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>(Arrays.asList(origin));
+        for (String word : text) {
+            if (!check.contains(word)) {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
