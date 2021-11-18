0d2d783fc35deeb4c1400f5353760408f494c925
NikolayKharkov <vtiko564@gmail.com>
2021-11-18 23:10:25 +0300

    2. Список адресов [#110062]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void testWhenSuccessfully() {
+        Profiles pf = new Profiles();
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Moscow", "Red Square", 27, 16)),
+                new Profile(new Address("New York", "Central Park", 49, 51))
+        );
+        List<Address> expected = List.of(
+                new Address("Moscow", "Red Square", 27, 16),
+                new Address("New York", "Central Park", 49, 51)
+        );
+        List<Address> result = pf.collect(profiles);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void testWhenFail() {
+        Profiles pf = new Profiles();
+        List<Profile> profiles = List.of(
+                new Profile(new Address("Moscow", "Red Square", 27, 16)),
+                new Profile(new Address("New York", "Central Park", 49, 51))
+        );
+        List<Address> expected = List.of(
+                new Address("Moscow", "Red Square", 27, 16)
+        );
+        List<Address> result = pf.collect(profiles);
+        assertNotEquals(expected, result);
+    }
+}
