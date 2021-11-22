7f2259a3a826fdd0259f174652ee67bfc5fcec8a
NikolayKharkov <vtiko564@gmail.com>
2021-11-22 21:30:14 +0300

    1. Метод .of [#70623]

--- src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -11,35 +11,39 @@ import static org.junit.Assert.assertThat;
 public class DepartmentsTest {
     @Test
     public void whenMissed() {
-        List<String> input = Arrays.asList("k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenNonChange() {
-        List<String> input = Arrays.asList("k1", "k1/sk1");
-        List<String> expect = Arrays.asList("k1", "k1/sk1");
+        List<String> input = List.of("k1", "k1/sk1");
+        List<String> expect = List.of("k1", "k1/sk1");
         List<String> result = Departments.fillGaps(input);
         assertThat(result, is(expect));
     }
 
     @Test
     public void whenSortByAsc() {
-        List<String> input = Arrays.asList("K1/SK1/SSK2", "K1/SK1", "K1/SK2",
-                "K2", "K1/SK1/SSK1", "K2/SK1/SSK1", "K2/SK1", "K1", "K2/SK1/SSK2");
-        String expect = "K1";
-        Departments.sortAsc(input);
-        assertThat(input.get(0), is(expect));
+        List<String> input = List.of("K2", "K2/SK1/SSK2", "K1/SK1/SSK1", "K1/SK2",
+                "K1/SK1/SSK2", "K1", "K2/SK1", "K2/SK1/SSK1", "K1/SK1");
+        List<String> expect = List.of("K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2",
+                "K1/SK2", "K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2");
+        List<String> result = Departments.fillGaps(input);
+        Departments.sortAsc(result);
+        assertThat(result, is(expect));
     }
 
     @Test
     public void whenSortByDesc() {
-        List<String> input = Arrays.asList("K1/SK1/SSK2", "K1/SK1", "K1/SK2",
-                "K2", "K1/SK1/SSK1", "K2/SK1/SSK1", "K2/SK1", "K1", "K2/SK1/SSK2");
-        String expect = "K2";
-        Departments.sortDesc(input);
-        assertThat(input.get(0), is(expect));
+        List<String> input = List.of("K2", "K2/SK1/SSK2", "K1/SK1/SSK1", "K1/SK2",
+                "K1/SK1/SSK2", "K1", "K2/SK1", "K2/SK1/SSK1", "K1/SK1");
+        List<String> expect = List.of("K2", "K2/SK1", "K2/SK1/SSK1", "K2/SK1/SSK2",
+                "K1", "K1/SK1", "K1/SK1/SSK1", "K1/SK1/SSK2", "K1/SK2");
+        List<String> result = Departments.fillGaps(input);
+        Departments.sortDesc(result);
+        assertThat(result, is(expect));
     }
 }
